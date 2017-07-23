41ff8281ce7bfff0de779808c170e7265fa0db77
Issac Lee <issactoast@gmail.com>
2017-07-23 00:34:03 -0500

    r,l,c added

--- R/div.R
+++ R/div.R
@@ -3,10 +3,34 @@
 #' This will generate the html syntax for align center and insert image
 #'
 #' @export
-insertDivAddin <- function() {
+insertDivAddin_c <- function() {
   rstudioapi::insertText("
-	<div align=\"center\"> \n
-		![]() \n
-	</div>
-  ")
+<div align=\"center\">
+![](){width=690}
+</div>
+")
+}
+#' Insert div algin left image
+#'
+#' This will generate the html syntax for align left and insert image
+#'
+#' @export
+insertDivAddin_l <- function() {
+  rstudioapi::insertText("
+<div align=\"left\">
+![](){width=690}
+</div>
+")
+}
+#' Insert div algin center image
+#'
+#' This will generate the html syntax for align right and insert image
+#'
+#' @export
+insertDivAddin_r <- function() {
+  rstudioapi::insertText("
+<div align=\"right\">
+![](){width=690}
+</div>
+")
 }
